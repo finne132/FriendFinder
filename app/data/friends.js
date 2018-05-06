045f7bd40ec115b0f62d42e05992dd4353640a37
Joe Finny <finne132@umn.edu>
2018-05-06 11:56:36 -0500

    made friend data structure more readable and labeled

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,7 +5,18 @@ var friends = [
 	{
 		name: 'Person 1',
 		photo: 'http://www.placepuppy.net/400/250',
-		scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+		scores: [
+            1, //response to question 1
+            1, //response to question 2
+            1, //response to question 3
+            1, //response to question 4
+            1, //response to question 5
+            1, //response to question 6
+            1, //response to question 7
+            1, //response to question 8
+            1, //response to question 9
+            1  //response to question 10
+        ]
 	}];
 
 // export 
