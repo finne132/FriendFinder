47feea0678e3bfe5a8b4360d1223e2d8f79230a9
Joe Finny <finne132@umn.edu>
2018-05-06 11:40:52 -0500

    added second route for the survey

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -7,5 +7,7 @@ module.exports = function(app){
 	app.get('/', function(request, result){
 		result.sendFile(path.join(__dirname, '../public/home.html'));
 	});
-
+    app.get('/survey', function(request, result){
+		result.sendFile(path.join(__dirname, '../public/survey.html'));
+	});
 };
