1098f1470aa2a22f639e62aff1e40e2e12946f62
Joe Finny <finne132@umn.edu>
2018-05-06 11:52:19 -0500

    followed the basic structure for friends

---
+++ app/data/friends.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// populate this with data 
+var friends = [
+	{
+		name: 'Person 1',
+		photo: 'http://www.placepuppy.net/400/250',
+		scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+	}];
+
+// export 
+module.exports = friends;
